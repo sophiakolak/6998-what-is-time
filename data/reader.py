e4e3cbf8476280ce91b7d3c31a7fccadd4178db4
Derek Wacks <derekwacks@DWs-MacBook-Pro.home>
2020-11-21 11:05:20 -0500

    beginning temp binning code

--- data/reader.py
+++ data/reader.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 data = pd.read_csv('wfC318/Day_4/Raw_files_csv/combined_behavior_and_s.csv', header=None)
 #data.info()
@@ -15,11 +16,36 @@ for i, j in data.iterrows():
     print(i, j)
 print(data.at[4,7])
 print(data.at[5,7])
-print(data.at[6,7])
 print(data.loc[4,7])
 print(data.loc[5,7])
 """
 
+
+time = data[1]
+odor = data[10]
+distance = data[7]
+lap = data[9]
+cell = data.loc[:, 12:(len(data.columns)-1)]  # neuron data
+print(cell)
+
+n_cell = len(data.columns)-12
+nlap = max(lap)
+print("LEN", n_cell)
+# find way to find last lap
+# in matlab, used find() to find row with the most number of equal components
+# i.e. the row that was previously selected
+# iLast(max())  # find way to find last lap --> just use counter?
+
+lapDistance = []
+for i in range(0, nlap):
+    #lapDistance.append( distance(iLap(i),iLap(i+1)) ) range in iLap
+
+"""
+ilast =
+np.zeros( define size )
+bins = pd.DataFrame( zero'd bins )
+
+# Remove rows where mouse is licking for reward past 4000
 counter=0
 for i in range(0, length):  # for every row of data
     if data.at[i, 7]>4000:  # if the distance > 4000
@@ -28,19 +54,4 @@ for i in range(0, length):  # for every row of data
         data.pop(i)  # remove that row
 
 print("New length:", len(data))
-
-
-"""
-#with open('wfC318/Day_0/Raw_files_csv/S.csv', newline='\n') as csvfile:
-with open('wfC318/Day_0/Raw_files_csv/combined_behavior_and_s.csv', newline='\n') as csvfile:
-    dreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    counter = 0
-    matrix = []
-    for row in dreader:
-        matrix.append(row)
-        if counter<5:
-            print(', '.join(row))
-            print("adding:", row)
-            counter=counter+1
-
 """
