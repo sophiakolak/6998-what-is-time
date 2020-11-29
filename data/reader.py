3a20f9554257875b724dee3de219d7af69d20b5a
Derek Wacks <derekwacks@DWs-MacBook-Pro.home>
2020-11-29 15:42:42 -0500

    sped up with lambda fn

--- data/reader.py
+++ data/reader.py
@@ -7,22 +7,6 @@ print(data.head())
 length = len(data)
 print(length)
 
-#print(data.sample(5))
-#print(data)
-#print(data.iloc[1])
-
-
-print("hello")
-
-"""
-for i, j in data.iterrows():
-    print(i, j)
-print(data.at[4,7])
-print(data.at[5,7])
-print(data.loc[4,7])
-print(data.loc[5,7])
-"""
-
 
 time = data[1]
 odor = data[10]
@@ -31,7 +15,7 @@ lap = data[9]
 # neuron data starts at column "13", indexed from 0 so the first column of neuron data is data[12]
 cell = data.loc[:, 12:(len(data.columns)-1)]  # neuron data
 #print(cell)
-print("TEST MAX N1:", max(data[12]), " N2:", max(data[13]))
+#print("TEST MAX N1:", max(data[12]), " N2:", max(data[13]))
 
 n_cell = len(data.columns)-12
 nlap = max(lap)
@@ -43,37 +27,45 @@ nlap = max(lap)
 
 
 lap_dictionary = {}
-for i in range(0, max(lap)): # for every lap
+for i in range(0, max(lap)):  # for every lap
     lap_dictionary[i] = data.loc[data[9] == i]  # associate all lap data with that lap
 #print(lap_dictionary)
 
 # create empty row to be used as neuron data
 empty_row = {}
-for k in range(0,459):  # 459 columns
+for k in range(0, 459):  # 459 columns
     empty_row[k] = 0.0
 print("empty_row!", len(empty_row), empty_row)
 
+counter = 0
 test_dictionary_for_laps = {}
 for i in lap_dictionary:  # for every trial
+
     #print("Lap number:", i)
 
     # average columns in that trial to test (eventually, in bin instead)
     current_row = pd.DataFrame(empty_row, index=[0])  # create pandas dataframe from a row of 0's
     print(i)
     for j in range(12, 471):  # for all neuron columns
-        print(j)
+        #print(j, len(data[j]))
+
         # setting value to 1 if val>0, 0 otherwise
         # if we really have to do this, find a different way
+
+        data[j].apply(lambda x: 1 if x>0 else 0)
+
+        """
         for l in range(0, len(data[j])):
             print(l)
             if data[j][l] > 0:
                 data.at[j, l] = 1  # replaces float value with binary --> this is crazy slow
                 #print("hello!")
-
+        """
         current_row[j-12] = data[j].mean()  # fill each column spot in that row with the mean of all values in that column
         # note: because we set values, this mean becomes frequency relative to that trial
 
     test_dictionary_for_laps[i] = current_row  # set dictionary item equal to that row
+    counter=counter+1
 
 print(test_dictionary_for_laps)
 
