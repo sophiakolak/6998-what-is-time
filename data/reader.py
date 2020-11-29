623471be7ee39612e823f8fd5571ff7847c88568
Derek Wacks <derekwacks@DWs-MacBook-Pro.home>
2020-11-29 16:58:12 -0500

    progress

--- data/reader.py
+++ data/reader.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import time as t
 
 data = pd.read_csv('wfC318/Day_4/Raw_files_csv/combined_behavior_and_s.csv', header=None)
 #data.info()
@@ -25,6 +26,12 @@ nlap = max(lap)
 # i.e. the row that was previously selected
 # iLast(max())  # find way to find last lap --> just use counter?
 
+#####
+# Changing neuron firing data to 0,1 binary
+for j in range(12, 471):  # for all neuron columns
+    # setting value to 1 if val>0, 0 otherwise
+    data[j].apply(lambda x: 1 if x > 0 else 0)
+#####
 
 lap_dictionary = {}
 for i in range(0, max(lap)):  # for every lap
@@ -37,22 +44,19 @@ for k in range(0, 459):  # 459 columns
     empty_row[k] = 0.0
 print("empty_row!", len(empty_row), empty_row)
 
-counter = 0
+flagCounter = 0
 test_dictionary_for_laps = {}
+start = t.time()
 for i in lap_dictionary:  # for every trial
 
     #print("Lap number:", i)
-
     # average columns in that trial to test (eventually, in bin instead)
     current_row = pd.DataFrame(empty_row, index=[0])  # create pandas dataframe from a row of 0's
-    print(i)
+    #print("RESET:", current_row)  # properly resetting
+    print(i)  # count 98 trial laps
     for j in range(12, 471):  # for all neuron columns
-        #print(j, len(data[j]))
-
         # setting value to 1 if val>0, 0 otherwise
         # if we really have to do this, find a different way
-
-        data[j].apply(lambda x: 1 if x>0 else 0)
 
         """
         for l in range(0, len(data[j])):
@@ -61,12 +65,18 @@ for i in lap_dictionary:  # for every trial
                 data.at[j, l] = 1  # replaces float value with binary --> this is crazy slow
                 #print("hello!")
         """
-        current_row[j-12] = data[j].mean()  # fill each column spot in that row with the mean of all values in that column
+        #current_row[j - 12] = data[j].mean()
+        current_row[j-12] = lap_dictionary[i][j].mean()  # fill each column spot in that row with the mean of all values in that column
         # note: because we set values, this mean becomes frequency relative to that trial
 
-    test_dictionary_for_laps[i] = current_row  # set dictionary item equal to that row
-    counter=counter+1
+    if flagCounter < 5:
+        print(current_row)
+        flagCounter += 1
 
+    test_dictionary_for_laps[i] = current_row  # set dictionary item equal to that row
+
+end = t.time()
+print("TIME:", end-start)
 print(test_dictionary_for_laps)
 
 """
@@ -84,3 +94,9 @@ for i in range(0, length):  # for every row of data
 
 print("New length:", len(data))
 """
+
+index_of_maxs = []
+for row in test_dictionary_for_laps:
+    index_of_maxs.append(test_dictionary_for_laps[row].idxmax())  # something's messed up here
+
+print(len(index_of_maxs), index_of_maxs)
