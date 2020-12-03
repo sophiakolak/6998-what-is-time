7882edac4468fda707d20312b09356510b4e50c1
Derek Wacks <djw2157@columbia.edu>
2020-12-03 18:26:42 -0500

    generalized some hardcoded variables

--- data/reader.py
+++ data/reader.py
@@ -4,17 +4,20 @@ import time as t
 
 ##########
 # Things to set:
-filename = 'wfC318/Day_4/Raw_files_csv/combined_behavior_and_s.csv'
+#filename = 'wfC318/Day_4/raw_files_csv/combined_behavior_and_s_COPY.csv'
+filename = 'wfC318/Day_4/raw_files_csv/combined_behavior_and_s.csv'
+#filename = 'wfC318/Day_0/Raw_files_csv/combined_behavior_and_s_COPY.csv'
+
 
 # How many trials we are looking at (ex: 31 for day 0, 34 for day 4)
-# trial_count = max(lap) # all of them
-trial_count = 34
+trial_count = 31
+#trial_count = max(lap)  # all of them
 
 ##########
 
 
 data = pd.read_csv(filename, header=None)
-print(data.head())
+print("Prev:", data.head())
 print("LEN1:", len(data))
 data = data[data[7] <= 4000]  # removing all rows where mouse is licking reward past 4000cm
 print("LEN2:", len(data))
@@ -27,12 +30,15 @@ lap = data[9]
 # neuron data starts at column "13", indexed from 0 so the first column of neuron data is data[12]
 cell = data.loc[:, 12:(len(data.columns)-1)]  # neuron data
 
+
 #n_cell = len(data.columns)-12
 #nlap = max(lap)
 
 ##### Cleaning up data
 # Changing neuron firing data to 0,1 binary
-for j in range(12, 471):  # for all neuron columns
+num_of_cols = len(data.columns)
+
+for j in range(12, num_of_cols):  # for all neuron columns
     # setting value to 1 if val>0, 0 otherwise
     data[j].apply(lambda x: 1 if x > 0 else 0)
 #####
@@ -75,7 +81,7 @@ print("Longest Trial:", longest_trial, "Time: ", max_time)
 
 # create empty row to store neuron data frequencies
 empty_row = {}
-for k in range(0, 459):  # 459 columns
+for k in range(0, num_of_cols-13):  # 459 columns
     empty_row[k] = 0.0
 
 def findMeanValues():
@@ -87,17 +93,7 @@ def findMeanValues():
         # average columns in that trial to test (eventually, in bin instead)
         current_row = pd.DataFrame(empty_row, index=[0])  # create pandas dataframe from a row of 0's (reset every lap)
         print(i)  # count 98 trial laps
-        for j in range(12, 471):  # for all neuron columns
-            """
-            # setting value to 1 if val>0, 0 otherwise
-            # if we really have to do this, find a different way
-    
-            for l in range(0, len(data[j])):
-                print(l)
-                if data[j][l] > 0:
-                    data.at[j, l] = 1  # replaces float value with binary --> this is crazy slow
-                    #print("hello!")
-            """
+        for j in range(12, num_of_cols):  # for all neuron columns
             #current_row[j - 12] = data[j].mean()
             current_row[j-12] = lap_dictionary[i][j].mean()  # fill each column spot in that row with the mean of all values in that column
             # note: because we set values to (0,1), this mean becomes frequency relative to that trial
@@ -132,8 +128,8 @@ for i in range(0, length):  # for every row of data
 print("New length:", len(data))
 """
 
-index_of_maxs = []
-for row in test_dictionary_for_laps:
-    index_of_maxs.append(test_dictionary_for_laps[row].idxmax())  # something's messed up here
+#index_of_maxs = []
+#for row in test_dictionary_for_laps:
+#    index_of_maxs.append(test_dictionary_for_laps[row].idxmax())  # something's messed up here
 
-print(len(index_of_maxs), index_of_maxs)
+#print(len(index_of_maxs), index_of_maxs)
